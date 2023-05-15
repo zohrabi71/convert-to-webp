64d46f56ed616f93dc4e85df7608098d78d3ab0d
mohammad <zohrabi.dev@gmail.com>
2023-05-15 15:27:52 +0330

    add styles

---
+++ app.js
@@ -0,0 +1,40 @@
+async function convertToWebp(file, quality = 0.8) {
+  const img = new Image();
+  img.src = URL.createObjectURL(file);
+
+  await new Promise((resolve) => {
+    img.onload = resolve;
+  });
+
+  const canvas = document.createElement("canvas");
+  canvas.width = 960; // Set desired width
+  canvas.height = 540; // Set desired height
+  const context = canvas.getContext("2d");
+
+  // Draw image on canvas with specified dimensions
+  context.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+  // Convert canvas to data URL with WebP format and specified quality
+  const webpDataUrl = canvas.toDataURL("image/webp", quality);
+
+  // Convert data URL to blob
+  const webpBlob = await fetch(webpDataUrl).then((res) => res.blob());
+
+  return webpBlob;
+}
+
+const convertButton = document.getElementById("convert-button");
+convertButton.addEventListener("click", async () => {
+  const fileInput = document.getElementById("img-file-input");
+  const file = fileInput.files[0];
+
+  // Need to upload a file
+  if(!file) {
+    return alert('Select a file please!')
+  }
+
+  const webpBlob = await convertToWebp(file);
+  const downloadButton = document.getElementById("download-converted-img-btn");
+  downloadButton.href = URL.createObjectURL(webpBlob);
+  downloadButton.style.display = "inline-block"; // Show download button after conversion
+});
